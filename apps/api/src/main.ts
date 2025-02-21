21a89a81bf8c36fff2a09d9f6cae5abd6ff4f784
cloneable01 <kevin.kidley@gmail.com>
2025-02-21 23:16:21 +0700

    [fix]: main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,7 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://hyperhire-snowy.vercel.app'],
+    origin: [
+      'http://localhost:3000',
+      'https://hyperhire-snowy.vercel.app',
+      'https://hyperhire-git-main-cloneable01s-projects.vercel.app',
+      'https://hyperhire-api-dun.vercel.app',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: false,
     allowedHeaders: ['Content-Type', 'Accept'],
