3853d5d7e6592b507958b5ee6828ad57ca3b9e6a
cloneable01 <kevin.kidley@gmail.com>
2025-02-25 00:10:36 +0700

    update main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,23 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-import * as serverless from 'serverless-http';
+// import * as express from 'express';
+// import * as serverless from 'serverless-http';
 
-const app = express();
+// const app = express();
 
 async function bootstrap() {
-  const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(app));
+  const nestApp = await NestFactory.create(AppModule);
 
   nestApp.enableCors({
     origin: '*',
   });
 
-  if (process.env.NODE_ENV === 'development') {
-    await nestApp.listen(8000);
-  } else {
-    await nestApp.init();
-  }
+  await nestApp.listen(8000);
 }
 bootstrap();
-export const handler = serverless(app);
